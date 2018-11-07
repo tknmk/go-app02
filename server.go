8cafbfeb835da34cc7a363d8adec9e339ad13d62
tknmk <kohdai.takanami@gmail.com>
2018-11-08 00:31:40 +0900

    [FIX] template fixed

--- server.go
+++ server.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-	"html/templaate"
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
+
+// map for getting base.html
+var templates map[string]*template.Template
 
 // Interface For Template
 type Template struct {
-	templates *template.Template
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 
-	// Add global methods if data is a map
-	if viewContext, isMap := data.(map[string]interface{}); isMap {
-		viewContext["reverse"] = c.Echo().Reverse
-	}
-
-	return t.templates.ExecuteTemplate(w, name, data)
+	return templates[name].ExecuteTemplate(w, name, data)
 }
 
 func main() {
@@ -28,12 +27,35 @@ func main() {
 	e := echo.New()
 
 	// set Renderer
-	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
-	}
+	t := &Template{}
+	e.Renderer = t
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
-	})
+	// set middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// set Static Files
+	e.Static("/public/css/", "./public/css")
+	e.Static("/public/js/", "./public/js/")
+	e.Static("/public/img/", "./public/img/")
+
+	e.GET("/", HandleTopGet)
+
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func init() {
+	loadTemplates()
+}
+
+func loadTemplates() {
+	var baseTemplate = "templates/main/base.html"
+	templates = make(map[string]*template.Template)
+
+	templates["Topmain"] = template.Must(template.ParseFiles(
+		baseTemplate, "templates/top/main.html", "templates/main/header.html", "templates/main/footer.html"))
+}
+
+func HandleTopGet(c echo.Context) error {
+	return c.Render(http.StatusOK, "Topmain", "OOOOOOKKKKKKKK")
 }
